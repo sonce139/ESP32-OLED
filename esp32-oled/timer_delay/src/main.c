3f3a8d5811a9175dfefd1540a118955d4a474ecf
SoniC <62106735+sonce139@users.noreply.github.com>
2021-04-02 10:02:35 +0700

    Update main.c

--- esp32-oled/timer_delay/src/main.c
+++ esp32-oled/timer_delay/src/main.c
@@ -9,11 +9,11 @@ Faculty of Computer Engineering - UIT
 #include "driver/timer.h"
 
 /* TIMER_BASE_CLK = 80 MHZ */ 
-// sample period interval for the timer 0 (1/TIMER_PERIOD = Timer frequency in HZ)
-#define TIMER_PERIOD   1 
+// delay time
+#define TIMER_PERIOD   2 
 
 #define TIMER_SENSOR TIMER_0        // Timer for counting 
-#define TIMER_DIVIDER         8     // Hardware timer clock divider
+#define TIMER_DIVIDER         80     // Hardware timer clock divider
 //convert counter value to seconds
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER) 
 #define TEST_WITHOUT_RELOAD   0     // testing will be done without auto reload
@@ -73,12 +73,8 @@ void conf_timer(){
 
 void timer_task(void *pvParameter)
 {
-    while (1) {
-        // Sleep until the ISR gives us something to do, if nothing is recieved then waits forever
-        uint32_t temp = ulTaskNotifyTake(pdFALSE, portMAX_DELAY); 
-        // if ISR give us something to do then print the following message 
+    while (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) 
         printf("Timer task!!! %d seconds has passed\n",TIMER_PERIOD);
-    }
 }
 
 
